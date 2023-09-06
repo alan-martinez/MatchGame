fc528ff5ef26ce0fb05937b54cb4091f6e08f472
alan-martinez <alan.martinez5691@alumnos.udg.mx>
2023-09-06 16:56:08 -0600

    ini

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -18,6 +18,8 @@ namespace MatchGame
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window
     {
         public MainWindow()
